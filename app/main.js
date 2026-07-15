7e2ba52690c7b013cfd0b12a014c14285a99bdb5
Subham Sahoo <andharalauri@gmail.com>
2026-07-15 17:01:24 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -10,16 +10,34 @@ const rl = readline.createInterface({
 rl.prompt();
 
 rl.on("line", (command) => {
+
   if (command === 'exit') {
     rl.close();
     return;
   } else if (command.startsWith("echo")) {
     console.log(command.slice(5))
     rl.prompt();
-  } else {
+  } else if (command.startsWith("type")) {
+    let isBuiltin = checkBuiltin(command.slice(5));
+    if(isBuiltin){
+      console.log(`${command.slice(5)} is a shell builtin`)
+    }else{
+      console.log(`type: ${command.slice(5)}: not found`)
+    }
+    rl.prompt()
+  }
+  else {
     console.error(`${command}: command not found`);
     rl.prompt();
   }
 });
+
+function checkBuiltin(str) {
+  const builtins = ["type", "exit", "echo"]
+  for (let el of builtins) {
+    if (el === str) return true
+  }
+  return false
+}
 
 // REPL -> Read Eval Print Loop
